5f5a0f0a2a39dc5e6a8f10e875701037400ade91
Alexandre Carvalho <alx_drd@hotmail.com>
2020-10-30 15:48:30 -0300

    Start project APP Sorteio!

---
+++ app/src/main/java/com/primeiroappsorteio/alexandrecarvalho97/MainActivity.java
@@ -0,0 +1,34 @@
+package com.primeiroappsorteio.alexandrecarvalho97;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void sortearNumero(View view){
+        TextView texto = findViewById(R.id.textoResultado);
+
+        int numero = new Random().nextInt(11);
+
+        texto.setText("Número: " + numero);
+    }
+
+    public void alterarTexto(View view){
+
+        TextView texto = findViewById(R.id.textoCliqueAqui);
+        texto.setText("Obrigado por participar!");
+
+    }
+
+}
